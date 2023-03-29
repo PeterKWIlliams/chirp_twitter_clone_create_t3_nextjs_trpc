f54b3ccd10c1da2ae6164eae36c065f61c2081d7
Peter Williams <peterkennethwilliams@gmail.com>
2023-03-29 02:13:29 +0100

    added Next image added styles to main oage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,12 @@ import {
 } from "@clerk/clerk-react";
 import { SignUp } from "@clerk/clerk-react";
 import { SignOutButton, UserButton } from "@clerk/nextjs";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 import { api, RouterOutputs } from "~/utils/api";
+import Image from "next/image";
 
 type PostWithUser = RouterOutputs["post"]["getAll"][number];
 
@@ -19,16 +23,21 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img
+      <Image
         src={author.profileImageUrl}
         alt="profile picture"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
 
       <div className=" flex flex-col text-slate-400">
         <div className="flex gap-2  text-slate-300">
           <span>{`@${author.username}`}</span>
-          <span className="font-thin">{` · 1 hour ago`}</span>
+
+          <span className="font-thin">{` · ${dayjs(
+            post.createdAt
+          ).fromNow()}`}</span>
         </div>
 
         <span>{post.content}</span>
@@ -43,10 +52,12 @@ const CreatePostWizard = () => {
   if (!user) return null;
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis"
