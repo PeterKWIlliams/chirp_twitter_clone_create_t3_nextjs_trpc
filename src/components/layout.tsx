375ba14c267ca8503392583b1a450c00e14718a9
Peter Williams <peterkennethwilliams@gmail.com>
2023-04-02 23:11:50 +0100

    changes made

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
+      <div className="flex-col">
+        <Link href={"/index"}></Link>
+      </div>
       <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
         {props.children}
       </div>
